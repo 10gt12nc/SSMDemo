ab70f7da9c98a267df985e7e2f75d98aea6f140b
10gt12nc <60815760+10gt12nc@users.noreply.github.com>
2020-11-19 12:24:09 +0800

    1119

--- src/test/java/com/lin/test/BaseTest.java
+++ src/test/java/com/lin/test/BaseTest.java
@@ -3,10 +3,15 @@ package com.lin.test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+
 //spring添加junit測試環境
 @RunWith(SpringJUnit4ClassRunner.class)
 //加載配置文件
 @ContextConfiguration(locations = {"classpath:app-context.xml"})
+@WebAppConfiguration() //設定web專案的環境，如果是Web專案，必須配置該屬性，否則無法獲取
 public class BaseTest {
 
 }
--- src/test/java/com/lin/test/SpringTest.java
+++ src/test/java/com/lin/test/SpringTest.java
@@ -3,20 +3,25 @@ package com.lin.test;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.lin.test.BaseTest;
+import org.springframework.stereotype.Component;
 
+import com.lin.test.BaseTest;
 
 public class SpringTest extends BaseTest {
 
+	// @Transactional //標明此方法需使用事務
 	@Test
-	void springTest() {
+	public void springTest() {
+		// 要public 不能void  say() 也要加註解 
+		//整個專案 run as  junit test
 		String path = "classpath*:/app-context.xml";
 		ApplicationContext ac = new ClassPathXmlApplicationContext(path);
 		SpringTest st = (SpringTest) ac.getBean(SpringTest.class);
 		st.say();
 	}
 
-	void say() {
+	@Test
+	public void say() {
 		System.out.println("Hi~~~");
 
 	}
